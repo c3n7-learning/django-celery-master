4b0de45c75571da17f902e9c0f0e46202468710b
Timothy Karani <kartimothy@gmail.com>
2024-06-14 18:04:10 +0300

    18. Starting the Celery Worker

--- dcelery/dcelery/celery.py
+++ dcelery/dcelery/celery.py
@@ -3,7 +3,7 @@ from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dcelery.settings")
 app = Celery("dcelery")
-app.conf.from_object("django.conf:settings", namespace="CELERY")
+app.config_from_object("django.conf:settings", namespace="CELERY")
 
 
 @app.task
