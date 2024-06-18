822319c47cdc1c8b27647a107da63e7c091e9a2e
Timothy Karani <kartimothy@gmail.com>
2024-06-18 18:06:47 +0300

    50. Monitoring Service Status Including Custom Event Tracking and Alerting

--- celeryworker/celerytask.py
+++ celeryworker/celerytask.py
@@ -1,7 +1,28 @@
 from celery import Celery
+import sentry_sdk
+from datetime import timedelta
+import os
 
 app = Celery("task")
 app.config_from_object("celeryconfig")
 app.conf.imports = "newapp.tasks"
 
 app.autodiscover_tasks()
+
+sentry_sdk.init(
+    dsn=os.getenv("SENTRY_DSN"),
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for tracing.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+)
+
+app.conf.beat_schedule = {
+    "task1": {
+        "task": "newapp.tasks.check_webpage",
+        "schedule": timedelta(seconds=10),
+    }
+}
--- celeryworker/newapp/tasks.py
+++ celeryworker/newapp/tasks.py
@@ -1,11 +1,17 @@
+import os
+import requests
+
 from celery import shared_task
+from sentry_sdk import capture_exception
 
 
 @shared_task
-def task1():
-    return
-
-
-@shared_task
-def task2():
-    return
+def check_webpage():
+    print(f"SENTRY_DSN: {os.getenv('SENTRY_DSN')}")
+    try:
+        response = requests.get("http://127.0.0.1:8001")
+        if response.status_code != 200:
+            raise Exception("Website is down, oh no!")
+    except requests.exceptions.RequestException as e:
+        capture_exception(e)
+        # raise e
