1dfd94be48ab22c234ba0b633a38bb48025d0cf2
Timothy Karani <kartimothy@gmail.com>
2024-06-18 03:05:08 +0300

    40. Task Timeouts and Task Revoking (Using task time limits and timeouts)

--- dcelery/dcelery/celery_tasks/ex7_timeouts_revoking.py
+++ dcelery/dcelery/celery_tasks/ex7_timeouts_revoking.py
@@ -2,7 +2,22 @@ from dcelery.celery_config import app
 from time import sleep
 
 
-@app.task(queue="tasks", time_limit=5)
+@app.task(queue="tasks", time_limit=10)
 def my_task_long_running():
     sleep(6)
     return "Task completed successfully"
+
+
+def execute_tasks():
+    result = my_task_long_running.delay()
+    try:
+        task_result = result.get(timeout=40)
+    except TimeoutError:
+        print("Task timed out")
+
+    task = my_task_long_running.delay()
+    task.revoke(terminate=True)
+
+    # Sleep for a bit, we want the revokation to be processed
+    sleep(3)
+    print(f"Task Status: {task.status}")
